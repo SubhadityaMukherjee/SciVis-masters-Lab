b873dad99ba464ac66e8813389b4e9addc8bfa2a
Lonnekee <lonneke.pulles@gmail.com>
2021-12-11 14:35:22 +0100

    Finished_isolines

--- Smoke/isoline.cpp
+++ Smoke/isoline.cpp
@@ -61,21 +61,31 @@ void Isoline::marchingSquaresNonInterpolated()
 
 void Isoline::marchingSquaresInterpolated()
 {
+    std::vector<bool> binary;
+    binary.reserve(m_values.size());
+
+    // Create binary matrix based on m_isolinesRho
+    for (size_t j = 0U; j < (m_DIM - 1U); ++j)
+    {
+        for (size_t i = 0U; i < (m_DIM - 1U); ++i)
+        {
+            binary[i + m_DIM * j] = m_values[i + m_DIM * j] > m_isolineRho;
+        }
+    }
+
     // Loop over the bottom left corner of each square
     for (size_t j = 0U; j < (m_DIM - 1U); ++j)
     {
         for (size_t i = 0U; i < (m_DIM - 1U); ++i)
         {
-            // Insert code here...
-
-            // v0 to v1 designate the corners of the current square.
+            // v0 to v3 designate the corners of the current square.
             // Their values should be the corresponding indices in the 'm_values' container.
-            size_t const v0 = 0U; // placeholder value
-            size_t const v1 = 0U; // placeholder value
-            size_t const v2 = 0U; // placeholder value
-            size_t const v3 = 0U; // placeholder value
+            size_t const v0 = i + m_DIM * j; // bottom left
+            size_t const v1 = (i+1) + m_DIM * j; // bottom right
+            size_t const v2 = (i+1) + m_DIM * (j+1); // top right
+            size_t const v3 = i + m_DIM * (j+1); // top left
 
-            size_t tableIdx = 0U; // placeholder value
+            size_t tableIdx = ((m_values[v3] > m_isolineRho) << 3) + ((m_values[v2] > m_isolineRho) << 2) + ((m_values[v1] > m_isolineRho) << 1) + (m_values[v0] > m_isolineRho);
 
             // For drawing offset the cells a little to the right and up to make it match the scalar field.
             QVector2D const bottomLeft{static_cast<float>(i + 1U) * m_cellSideLength,
@@ -124,43 +134,84 @@ void Isoline::case1(QVector2D const &bottomLeft,
 }
 
 void Isoline::case2([[maybe_unused]] QVector2D const &bottomLeft,
-                    [[maybe_unused]] size_t const v0,
-                    [[maybe_unused]] size_t const v1,
-                    [[maybe_unused]] size_t const v2,
+                    size_t const v0,
+                    size_t const v1,
+                    size_t const v2,
                     [[maybe_unused]] size_t const v3)
 {
+    // General interpolation
+    float const deltaX = (m_isolineRho - m_values[v0]) / (m_values[v1] - m_values[v0]);
+    float const deltaY = (m_isolineRho - m_values[v1]) / (m_values[v2] - m_values[v1]);
+
+    // Add the start and end coordinates of the line,
+    // based on an offset of the location of the bottom-left corner
+    // and as a point somewhere along the length of the side of a cell/square.
+    m_vertices.emplace_back(bottomLeft + QVector2D{deltaX * m_cellSideLength, 0.0F});
+    m_vertices.emplace_back(bottomLeft + QVector2D{1.0F * m_cellSideLength, deltaY * m_cellSideLength});
 }
 
 void Isoline::case3([[maybe_unused]] QVector2D const &bottomLeft,
-                    [[maybe_unused]] size_t const v0,
-                    [[maybe_unused]] size_t const v1,
-                    [[maybe_unused]] size_t const v2,
-                    [[maybe_unused]] size_t const v3)
+                    size_t const v0,
+                    size_t const v1,
+                    size_t const v2,
+                    size_t const v3)
 {
+    // General interpolation
+    float const deltaLeft = (m_isolineRho - m_values[v0]) / (m_values[v3] - m_values[v0]);
+    float const deltaRight = (m_isolineRho - m_values[v1]) / (m_values[v2] - m_values[v1]);
+
+    // Add the start and end coordinates of the line,
+    // based on an offset of the location of the bottom-left corner
+    // and as a point somewhere along the length of the side of a cell/square.
+    m_vertices.emplace_back(bottomLeft + QVector2D{0.0F, deltaLeft * m_cellSideLength});
+    m_vertices.emplace_back(bottomLeft + QVector2D{1.0F * m_cellSideLength, deltaRight * m_cellSideLength});
 }
 
 void Isoline::case4([[maybe_unused]] QVector2D const &bottomLeft,
                     [[maybe_unused]] size_t const v0,
-                    [[maybe_unused]] size_t const v1,
-                    [[maybe_unused]] size_t const v2,
-                    [[maybe_unused]] size_t const v3)
+                    size_t const v1,
+                    size_t const v2,
+                    size_t const v3)
 {
+    // General interpolation
+    float const deltaTop = (m_isolineRho - m_values[v3]) / (m_values[v2] - m_values[v3]);
+    float const deltaRight = (m_isolineRho - m_values[v1]) / (m_values[v2] - m_values[v1]);
+
+    // Add the start and end coordinates of the line,
+    // based on an offset of the location of the bottom-left corner
+    // and as a point somewhere along the length of the side of a cell/square.
+    m_vertices.emplace_back(bottomLeft + QVector2D{deltaTop * m_cellSideLength, 1.0F * m_cellSideLength});
+    m_vertices.emplace_back(bottomLeft + QVector2D{1.0F * m_cellSideLength, deltaRight * m_cellSideLength});
 }
 
 void Isoline::case5([[maybe_unused]] QVector2D const &bottomLeft,
-                    [[maybe_unused]] size_t const v0,
-                    [[maybe_unused]] size_t const v1,
-                    [[maybe_unused]] size_t const v2,
-                    [[maybe_unused]] size_t const v3)
+                    size_t const v0,
+                    size_t const v1,
+                    size_t const v2,
+                    size_t const v3)
 {
+    // Case 1
+    Isoline::case1(bottomLeft, v0, v1, v2, v3);
+
+    // And case 4
+    Isoline::case4(bottomLeft, v0, v1, v2, v3);
 }
 
 void Isoline::case6([[maybe_unused]] QVector2D const &bottomLeft,
-                    [[maybe_unused]] size_t const v0,
-                    [[maybe_unused]] size_t const v1,
-                    [[maybe_unused]] size_t const v2,
-                    [[maybe_unused]] size_t const v3)
+                    size_t const v0,
+                    size_t const v1,
+                    size_t const v2,
+                    size_t const v3)
 {
+    // General interpolation
+    float const deltaXTop = (m_isolineRho - m_values[v3]) / (m_values[v2] - m_values[v3]);
+    float const deltaXBottom = (m_isolineRho - m_values[v0]) / (m_values[v1] - m_values[v0]);
+
+    // Add the start and end coordinates of the line,
+    // based on an offset of the location of the bottom-left corner
+    // and as a point somewhere along the length of the side of a cell/square.
+    m_vertices.emplace_back(bottomLeft + QVector2D{deltaXTop * m_cellSideLength, 1.0F * m_cellSideLength});
+    m_vertices.emplace_back(bottomLeft + QVector2D{deltaXBottom * m_cellSideLength, 0.0F});
 }
 
 void Isoline::case7([[maybe_unused]] QVector2D const &bottomLeft,
@@ -169,6 +220,15 @@ void Isoline::case7([[maybe_unused]] QVector2D const &bottomLeft,
                     [[maybe_unused]] size_t const v2,
                     [[maybe_unused]] size_t const v3)
 {
+    // General interpolation
+    float const deltaX = (m_isolineRho - m_values[v3]) / (m_values[v2] - m_values[v3]);
+    float const deltaY = (m_isolineRho - m_values[v0]) / (m_values[v3] - m_values[v0]);
+
+    // Add the start and end coordinates of the line,
+    // based on an offset of the location of the bottom-left corner
+    // and as a point somewhere along the length of the side of a cell/square.
+    m_vertices.emplace_back(bottomLeft + QVector2D{deltaX * m_cellSideLength, 1.0F * m_cellSideLength});
+    m_vertices.emplace_back(bottomLeft + QVector2D{0.0F, deltaY * m_cellSideLength});
 }
 
 void Isoline::case8([[maybe_unused]] QVector2D const &bottomLeft,
@@ -177,6 +237,8 @@ void Isoline::case8([[maybe_unused]] QVector2D const &bottomLeft,
                     [[maybe_unused]] size_t const v2,
                     [[maybe_unused]] size_t const v3)
 {
+    // Same as case 7, but reversed: the minusses in the deltaX/-Y calculations cancel out, though.
+    Isoline::case7(bottomLeft, v0, v1, v2, v3);
 }
 
 void Isoline::case9([[maybe_unused]] QVector2D const &bottomLeft,
@@ -185,6 +247,8 @@ void Isoline::case9([[maybe_unused]] QVector2D const &bottomLeft,
                     [[maybe_unused]] size_t const v2,
                     [[maybe_unused]] size_t const v3)
 {
+    // Same as case 6
+    Isoline::case6(bottomLeft, v0, v1, v2, v3);
 }
 
 void Isoline::case10([[maybe_unused]] QVector2D const &bottomLeft,
@@ -193,6 +257,9 @@ void Isoline::case10([[maybe_unused]] QVector2D const &bottomLeft,
                      [[maybe_unused]] size_t const v2,
                      [[maybe_unused]] size_t const v3)
 {
+    // Same as case 8 and 13, or cases 2 and 7
+    Isoline::case2(bottomLeft, v0, v1, v2, v3);
+    Isoline::case7(bottomLeft, v0, v1, v2, v3);
 }
 
 void Isoline::case11([[maybe_unused]] QVector2D const &bottomLeft,
@@ -201,6 +268,8 @@ void Isoline::case11([[maybe_unused]] QVector2D const &bottomLeft,
                      [[maybe_unused]] size_t const v2,
                      [[maybe_unused]] size_t const v3)
 {
+    // Same as case 4
+    Isoline::case4(bottomLeft, v0, v1, v2, v3);
 }
 
 void Isoline::case12([[maybe_unused]] QVector2D const &bottomLeft,
@@ -209,6 +278,8 @@ void Isoline::case12([[maybe_unused]] QVector2D const &bottomLeft,
                      [[maybe_unused]] size_t const v2,
                      [[maybe_unused]] size_t const v3)
 {
+    // Same as case 3
+    Isoline::case3(bottomLeft, v0, v1, v2, v3);
 }
 
 void Isoline::case13([[maybe_unused]] QVector2D const &bottomLeft,
@@ -217,6 +288,8 @@ void Isoline::case13([[maybe_unused]] QVector2D const &bottomLeft,
                      [[maybe_unused]] size_t const v2,
                      [[maybe_unused]] size_t const v3)
 {
+    // Same as case 2
+    Isoline::case2(bottomLeft, v0, v1, v2, v3);
 }
 
 void Isoline::case14([[maybe_unused]] QVector2D const &bottomLeft,
@@ -225,6 +298,7 @@ void Isoline::case14([[maybe_unused]] QVector2D const &bottomLeft,
                      [[maybe_unused]] size_t const v2,
                      [[maybe_unused]] size_t const v3)
 {
+    Isoline::case1(bottomLeft, v0, v1, v2, v3);
 }
 
 void Isoline::case15([[maybe_unused]] QVector2D const &bottomLeft,

f9c1bcfa07d7cfb694ead2c19600257f50abed9b
Lonnekee <lonneke.pulles@gmail.com>
2021-12-17 16:13:26 +0100

    Worked some more on task 1

--- Assignment2/pre-integration.cpp
+++ Assignment2/pre-integration.cpp
@@ -19,7 +19,6 @@ float const colorNode2[3U] = { 1.0F, 0.0F, 0.0F };  // red
 float composedColor[4U] = { 0.0F };
 // Number of maximum raycasting samples per ray
 const float sampleNum = 150.0F;
-const float t = 1 / sampleNum;
 
 // TODO: modify the transferFunction and add necessary functions
 
@@ -94,62 +93,91 @@ void writeImage(std::vector<std::array<float, 4U>> image,std::string filename="p
     }
 }
 
-void accumulation(float value, float sampleRatio, inout vec4 composedColor)
+/**
+ * Correct opacity for the current sampling rate
+ *
+ * @param alpha: input opacity.
+ * @param samplingRatio: the ratio between current sampling rate and the original.
+ */
+float opacityCorrection(float alpha, float samplingRatio)
 {
-        vec4 color = transferFunction(value);              // color_CUR
-        color.a = opacityCorrection(color.a, sampleRatio); // alpha_CUR
+    float a_corrected = 1.0F - pow(1.0F - alpha, samplingRatio);
+    return a_corrected;
+}
 
-        // DONE: Implement Front-to-back blending
-    float alpha_i = composedColor.a;
-        
-    vec3 color_new = composedColor.xyz + (1.0 - alpha_i) * color.xyz * color.a; //
-    float alpha_new = alpha_i + (1.0 - alpha_i) * color.a;
-        
-    composedColor = vec4(color_new, alpha_new);
+/**
+ * Pre-integration composition, based on accumulation
+ *
+ * @param sample: current sample value.
+ * @param samplingRatio: the ratio between current sampling rate and the original. (ray step)
+ * @param composedColor: blended color (both input and output)
+ */
+std::array<float, 4U> accumulation(float transferValue, float samplingRatio, std::array<float, 4U> composedColor)
+{
+    // Get color from transfer function and correct opacity
+    std::array<float, 4U> color = transferFunction(transferValue);
+    color[3] = opacityCorrection(color[3], samplingRatio);
+    float alpha_i = composedColor[3];
+    
+    // Easier naming
+    std::array<float, 3U> composedXYZ;
+    std::copy(composedColor.begin(), composedColor.begin() + 3, composedXYZ.begin());
+    std::array<float, 3U> colorXYZ;
+    std::copy(color.begin(), color.begin() + 3, colorXYZ.begin());
+    
+    // Calculate new values
+    std::array<float, 3U> color_new;
+    std::transform(composedXYZ.begin(), composedXYZ.end(), colorXYZ.begin(), color_new.begin(), [alpha_i, color](float &first, float &second) {
+        return first + (1.0F - alpha_i) * color[3] * second;
+    }); // composedXYZ + (1.0F - alpha_i) * color[3] * colorXYZ;
+    float alpha_new = alpha_i + (1.0F - alpha_i) * color[3];
+    
+    // Store as result
+    std::copy(color_new.begin(), color_new.end(), composedColor.begin());
+    composedColor[3] = alpha_new;
+    
+    return composedColor;
+}
+
+std::array<float, 4U> preintegration(size_t xIdx, size_t yIdx)
+{
+    float s_f = static_cast<float>(xIdx);
+    float s_b = static_cast<float>(yIdx);
+    
+    std::array<float, 4U> composedColor;
+
+    for (size_t i = 0; i < sampleNum; ++i)
+    {
+        float samplingRatio = i / sampleNum;
+    
+        // 1. Sampling: interpolation of the two scalars
+        float s_L = s_b + samplingRatio * (s_f - s_b);
+
+        // 2. Classification: apply transfer function
+        std::array<float, 4U> color = transferFunction(s_L);
+
+        // 3. Compositing: perform pre-integration/accumulation
+        composedColor = accumulation(s_L, samplingRatio, composedColor);
+    }
+    
+    return composedColor;
 }
 
 int main()
 {
     // 2D color image
-    float color2D[256U][256U][4U] = { 0.0F };
-    
-    // TODO: add pre-integration table calculations
-
-    // TODO: convert obtained array to an image
+//    float color2D[256U][256U][4U] = { 0.0F };
+    std::vector<std::array<float, 4U>> image;
 
     for (size_t yIdx = 0U; yIdx < 256U; ++yIdx) {
         for (size_t xIdx = 0U; xIdx < 256U; ++xIdx) {
-		// 1. Interpolation of the two scalars
-		float s_f = static_cast<float>(xIdx);
-		float s_b = static_cast<float>(yIdx);
+            // TODO: add pre-integration table calculations
+            std::array<float, 4U> composedColor = preintegration(xIdx, yIdx);
 
-		float s_L = s_b + t / sampleNum * (s_f - s_b);
-		// 2. Apply transfer function
-		std::array<float, 4U> color = transferFunction(s_L);
-
-		// 3. Perform pre-integration/accumulation
-		
-
+            // DONE: convert obtained array to an image
+            image.emplace_back(composedColor);
         }
     }
-
-    // you can use writeImage function to save the image (as in the example) or
-    // use any libraries (e.g. ImageMagick or OpenCV) you would like
-    std::vector<std::array<float, 4U>> image;
-    for (size_t yIdx = 0U; yIdx < 256U; ++yIdx)
-        for (size_t xIdx = 0U; xIdx < 256U; ++xIdx){
-            std::array<float, 4U> color0 = transferFunction(static_cast<float>(yIdx));
-            image.emplace_back(color0);
-	}
-
-    
-    //for (size_t yIdx = 0U; yIdx < 256U; ++yIdx)
-    //    for (size_t xIdx = 0U; xIdx < 256U; ++xIdx)
-    //        image.emplace_back(std::array<float, 4U>{static_cast<float>(yIdx) / 255.0F,
-    //                                                 static_cast<float>(xIdx) / 255.0F,
-    //                                                 static_cast<float>(255U - xIdx) / 255.0F,
-     //                                                255.0F});
-     
     
     writeImage(image);
 

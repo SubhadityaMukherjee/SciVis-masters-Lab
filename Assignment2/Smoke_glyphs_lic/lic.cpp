6ea75ed4a5d80c09479d6c3f97ef8a22dbaad676
SubhadityaMukherjee <msubhaditya@gmail.com>
2022-01-15 14:47:57 -0800

    ass_2_part3_abit

--- Assignment2/Smoke_glyphs_lic/lic.cpp
+++ Assignment2/Smoke_glyphs_lic/lic.cpp
@@ -2,8 +2,12 @@
 
 #include <QDebug>
 
-#include <string>
-
+#include<vector>
+#include<numeric>
+#include <random>
+#include <algorithm>
+#include <iterator>
+#include <iostream>
 
 Lic::Lic(unsigned int xDim, unsigned int yDim)
     :
@@ -28,12 +32,18 @@ Lic::Lic(unsigned int xDim, unsigned int yDim)
 ///		normalize the vector field     ///
 void Lic::normalizeVectors(std::vector<float>* vectorField_x, std::vector<float>* vectorField_y)
 {
-
+ float totals_x = std::inner_product( vectorField_x->begin(), vectorField_x->end(), vectorField_x->begin(), 0 );
+ float totals_y = std::inner_product( vectorField_y->begin(), vectorField_y->end(), vectorField_y->begin(), 0 );
+ std::for_each(vectorField_x->begin(), vectorField_x->end(), [totals_x](float &c){ c /= totals_x; });
+ std::for_each(vectorField_y->begin(), vectorField_y->end(), [totals_y](float &c){ c /= totals_y; });
 }
+
 
 ///		make white noise as the LIC input texture     ///
 std::vector<float> Lic::generateNoiseTexture(std::vector<float> texture)
 {
+     srand(time(0));
+       generate(begin(texture), end(texture), rand);
     return texture;
 }
 
